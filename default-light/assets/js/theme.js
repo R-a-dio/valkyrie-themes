0b11e4616fbc1ec67dda68021ae2b56b9476427c
Wessie <github@wessie.info>
2024-02-28 23:08:30 +0000

    move templates from valkyrie repository

---
+++ default-light/assets/js/theme.js
@@ -0,0 +1,17 @@
+function toggleSongInfoDropdown(div) {
+    div.nextElementSibling.classList.toggle("is-hidden");
+}
+
+// Clear navbar search if it loses focus. There's probably a better way to do this.
+function clearNavbarSearchValue(event) {
+    parent = document.getElementById("search-bar");
+    originalTarget = event.originalTarget;
+
+    document.getElementById("content").addEventListener("click", (e) => {
+        if (!parent.contains(e.explicitOriginalTarget)) {
+            originalTarget.value = "";
+            htmx.trigger(originalTarget, "input");
+            document.body.removeEventListener("click", this, true);
+        }
+    })
+}
