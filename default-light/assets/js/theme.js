738a44dcce81aa465747a9b867d32ecf57169b0b
ixces <dev.null@need.moe>
2024-05-05 17:36:20 +0300

    js: fix firefox-only javascript after reverting commit

--- default-light/assets/js/theme.js
+++ default-light/assets/js/theme.js
@@ -1,10 +1,10 @@
 // Clear navbar search if it loses focus. There's probably a better way to do this.
 function clearNavbarSearchValue(event) {
     parent = document.getElementById("search-bar");
-    originalTarget = event.originalTarget;
+    originalTarget = event.target;
 
     document.getElementById("content").addEventListener("click", (e) => {
-        if (!parent.contains(e.explicitOriginalTarget)) {
+        if (!parent.contains(e.target)) {
             originalTarget.value = "";
             htmx.trigger(originalTarget, "input");
             document.body.removeEventListener("click", this, true);
