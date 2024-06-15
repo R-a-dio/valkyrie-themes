afb3ea0aabc49e246bc34d3cc9089d17f537c67b
Wessie <github@wessie.info>
2024-06-15 17:32:13 +0100

    default-light: compile with sass (seems to be a bulma update?)
    add generate.go for go generate usage (this still requires dart-sass
    to be installed and have bulma in the root directory)

---
+++ generate.go
@@ -0,0 +1,4 @@
+package themes
+
+//go:generate sass --style=compressed default-dark/assets/sass/dark.scss:default-dark/assets/css/bulma.min.css
+//go:generate sass --style=compressed default-light/assets/sass/light.scss:default-light/assets/css/bulma.min.css
